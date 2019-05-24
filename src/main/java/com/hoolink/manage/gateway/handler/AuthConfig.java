d56f18fa17bb689cd39771fb251039b08a67354f
许保锋 <yanwu0527@163.com>
2019-05-24 15:04:43 +0800

    提交: 1.过滤上传文件

--- src/main/java/com/hoolink/manage/gateway/handler/AuthConfig.java
+++ src/main/java/com/hoolink/manage/gateway/handler/AuthConfig.java
@@ -27,7 +27,8 @@ public class AuthConfig {
         // 校验手机验证码
         passOperations.add("manage-base.userController.verifyPhoneCode");
 
-
+        // 上传客户资源 (头像等)
+        passOperations.add("hoolink-ability.web.ObsController.uploadCustom");
     }
 
     public static final Set<String> getPassOperations() {
