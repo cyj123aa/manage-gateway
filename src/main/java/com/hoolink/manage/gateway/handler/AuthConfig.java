a94ccefe4ce0954cbfbe70c479bfb6816fd2d26a
韦敏 <15711036498@163.com>
2019-10-17 09:16:08 +0800

    提交白名单

--- src/main/java/com/hoolink/manage/gateway/handler/AuthConfig.java
+++ src/main/java/com/hoolink/manage/gateway/handler/AuthConfig.java
@@ -18,33 +18,29 @@ public class AuthConfig {
         passOperations = new HashSet<>();
         // 登录
         passOperations.add("/manage-base/web/user/login");
-        passOperations.add("/manage-base/userController/login");
-        passOperations.add("/manage-base/mobileUserController/login");
-        passOperations.add("/manage-base/roleController/getBaseMenu");
-        //登出
-        passOperations.add("/manage-base/userController/logout");
+        passOperations.add("/manage-base/mobile/user/login");
+        passOperations.add("/manage-base/web/role/getBaseMenu");
         // 获取Session，登录时鉴权用
-        passOperations.add("/manage-base/userController/getSessionUser");
-        passOperations.add("/manage-base/mobileUserController/getSessionUser");
+        passOperations.add("/manage-base/web/user/getSessionUser");
+        passOperations.add("/manage-base/mobile/user/getSessionUser");
         // 忘记密码
-        passOperations.add("/manage-base/userController/forgetPassword");
-        passOperations.add("/manage-base/mobileUserController/forgetPassword");
+        passOperations.add("/manage-base/web/user/forgetPassword");
+        passOperations.add("/manage-base/mobile/user/forgetPassword");
         // 获取验证码
-        passOperations.add("/manage-base/userController/getPhoneCode");
-        passOperations.add("/manage-base/userController/bindPhoneGetCode");
-        passOperations.add("/manage-base/mobileUserController/getPhoneCode");
-        passOperations.add("/manage-base/mobileUserController/bindPhoneGetCode");
+        passOperations.add("/manage-base/web/user/getPhoneCode");
+        passOperations.add("/manage-base/web/user/bindPhoneGetCode");
+        passOperations.add("/manage-base/mobile/user/getPhoneCode");
+        passOperations.add("/manage-base/mobile/user/bindPhoneGetCode");
         // 重置密码
-        passOperations.add("/manage-base/userController/resetPassword");
-        passOperations.add("/manage-base/mobileUserController/resetPassword");
+        passOperations.add("/manage-base/web/user/resetPassword");
+        passOperations.add("/manage-base/mobile/user/resetPassword");
         // 校验手机验证码
-        passOperations.add("/manage-base/userController/verifyPhoneCode");
-        passOperations.add("/manage-base/mobileUserController/verifyPhoneCode");
+        passOperations.add("/manage-base/web/user/verifyPhoneCode");
+        passOperations.add("/manage-base/mobile/user/verifyPhoneCode");
 
         // 文件
-        passOperations.add("/hoolink-ability/web/ObsController/uploadCustom");
-        passOperations.add("/hoolink-ability/web/ObsController/uploadManager");
-        passOperations.add("/manage-base/mobileUserController/logout");
+        passOperations.add("/hoolink-ability/web/obs/uploadCustom");
+        passOperations.add("/hoolink-ability/web/obs/uploadManager");
 
         // 需要登录但不需要鉴权的接口
         passOperationsWithoutAuth = new HashSet<>();
@@ -53,23 +49,23 @@ public class AuthConfig {
          */
 
         //获取基础信息
-        passOperationsWithoutAuth.add("/manage-base/personalCenterController/getManagerUserInfo");
+        passOperationsWithoutAuth.add("/manage-base/web/personalCenter/getManagerUserInfo");
         //修改密码
-        passOperationsWithoutAuth.add("/manage-base/userController/updatePassword");
-        passOperationsWithoutAuth.add("/manage-base/mobileUserController/updatePassword");
+        passOperationsWithoutAuth.add("/manage-base/web/user/updatePassword");
+        passOperationsWithoutAuth.add("/manage-base/mobile/user/updatePassword");
         //保存头像
-        passOperationsWithoutAuth.add("/manage-base/personalCenterController/updateImageId");
+        passOperationsWithoutAuth.add("/manage-base/web/personalCenter/updateImageId");
         //绑定手机号
-        passOperationsWithoutAuth.add("/manage-base/userController/bindPhone");
-        passOperationsWithoutAuth.add("/manage-base/mobileUserController/bindPhone");
+        passOperationsWithoutAuth.add("/manage-base/web/user/bindPhone");
+        passOperationsWithoutAuth.add("/manage-base/mobile/user/bindPhone");
         //用户退出
-        passOperationsWithoutAuth.add("/manage-base/userController/logout");
-        passOperationsWithoutAuth.add("/manage-base/mobileUserController/logout");
+        passOperationsWithoutAuth.add("/manage-base/web/user/logout");
+        passOperationsWithoutAuth.add("/manage-base/mobile/user/logout");
 
         //获取基础信息
-        passOperationsWithoutAuth.add("/manage-base/mobile/personalCenterController/getManagerUserInfo");
+        passOperationsWithoutAuth.add("/manage-base/mobile/personalCenter/getManagerUserInfo");
         //上传并保存头像
-        passOperationsWithoutAuth.add("/manage-base/mobile/personalCenterController/uploadImage");
+        passOperationsWithoutAuth.add("/manage-base/mobile/personalCenter/uploadImage");
     }
 
     public static final Set<String> getPassOperations() {
